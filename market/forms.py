0e2aee605734105694e3825dca2a096d2065d434
hakutoshiro <hakutoshiro1@gmail.com>
2023-09-05 20:47:20 +0530

    commiting updates

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Length,EqualTo,Email,DataRequired 
 
 class RegisterForm(FlaskForm):
-    username=StringField(label='User Name:')
-    email=StringField(label='Email Address:')
-    password1=StringField(label='Password:')
-    password2=StringField(label='Confirm Password:')
+    username=StringField(label='User Name:',validators=[Length(min=2,max=30),DataRequired()])
+    email=StringField(label='Email Address:',validators=[Email(),DataRequired()])
+    password1=PasswordField(label='Password:',validators=[Length(min=6),DataRequired()])
+    password2=PasswordField(label='Confirm Password:',validators=[EqualTo('password1'),DataRequired()])
     submit=SubmitField(label='Create Account')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app,db
+from flask import render_template,redirect,url_for
+from market.models import Item,User
 from market.forms import RegisterForm
+
 
 @app.route('/')
 @app.route('/home')
@@ -13,7 +14,20 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html',products=items)
 
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def register_page():
     form= RegisterForm()
+
+    if form.validate_on_submit():
+        user=User(
+            username=form.username.data,
+            email=form.email.data,
+            password_hash=form.password1.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    
+    if form.errors != {}:
+        for i in form.errors.values():
+            print(i)
     return render_template('register.html',form=form)
